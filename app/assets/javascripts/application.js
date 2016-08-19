3b7c06cc0e3512537e72a7e0f1b79e9e641a4679
미니똥미니 <ecovic94@gmail.com>
2016-08-19 16:28:30 +0900

    bower install materialize & jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
+//= require materialize
 //= require_tree .
+//= require turbolinks
+//
+$.turbo.use('turbolinks:load');
