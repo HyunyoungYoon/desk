fe6733a925af5c379bdd353a798a11e756b16596
미니똥미니 <ecovic94@gmail.com>
2016-08-24 21:09:15 +0900

    error fixed (has_many :agendas -> {include: :posts} comment)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
 
   has_many :agendas, through: :deskings
   has_many :deskings
-  has_many :agendas, -> {includes :posts}
+ # has_many :agendas, -> {includes :posts}
 end
 
