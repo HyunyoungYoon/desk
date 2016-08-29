b596430e795fd235f73dfad092f124d4406a4faf
hyunyoungyoon <yoonhy557@gmail.com>
2016-08-29 18:12:08 +0000

    seed file for agenda 기본소득논쟁

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-User.create!([
-  {email: "user1@snu.ac.kr", encrypted_password: "$2a$11$RNfTn0pBUB0Quq3xcfctMewk6nrYZ.SwIu/hjPNpDOevb9acM8hbG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: "2016-08-29 16:57:55", sign_in_count: 1, current_sign_in_at: "2016-08-29 16:57:55", last_sign_in_at: "2016-08-29 16:57:55", current_sign_in_ip: "121.133.34.151", last_sign_in_ip: "121.133.34.151", is_admin: true},
-  {email: "user2@snu.ac.kr", encrypted_password: "$2a$11$.rW9t1htpdgD1Xpbxzefj.yMiQiLSOX0cilc5kvCRKwujxbwKAWbe", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, is_admin: true},
-  {email: "user3@snu.ac.kr", encrypted_password: "$2a$11$KL7LGaKjZDfdKFPANG99xerFmtfTW3B/hi9jD6RZqNUohoTO8iN7W", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, is_admin: true},
-  {email: "user4@snu.ac.kr", encrypted_password: "$2a$11$RS8VY0u15V3WN5pt7U015eLKy7LBRU4Mc9zBYIWRRLb2wo7ynLX4u", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, is_admin: true}
-])
+User.create(email: 'user1@snu.ac.kr', :password => '12341234', :password_confirmation => '12341234')
 Agenda.create!([
-  {name: "기본 소득 제도", color: "", user_id: nil}
+  {name: "기본 소득 논쟁", color: "", user_id: 1}
 ])
 Comment.create!([
-  {content: "조성주는 좌파의 ‘진정성/거짓’이라는 가치판단의 도식을 우파의 ‘현실주의·정책/감상·이미지·비합리’라는 도식과 융합함으로써 ‘진정성=현실주의·정책/감상·이미지·비합리=거짓’이라는 새로운 형태의 도식을 만들어내고, 거기에 기초해서 자신을 ‘현실주의/정책론자/진보’라는 새롭게 등장한 포지션에 배치한다.", writer: nil, share: 0, post_id: 1}
+  {content: "기본 소득을 시대에 따라 변화하는, 혹은 변화해야하는 사회안전망의 모습에 비추어 평가한다.", writer: 1, share: 0, post_id: 1},
+  {content: "독일의 기본 소득 논의를 단계적으로 살펴볼 수 있다.\r\n이러이러한 점은 이해가 안된다. 이러이러한 점은 공감된다.", writer: 1, share: 0, post_id: 3}
 ])
 Post.create!([
-  {url: "https://www.facebook.com/jodeng78/posts/1069591639777202", title: "조성주 - 당에서 기본소득을 검토중이라고 한다. 김종인 대표가 기본소득을 언급한 후이고 갑자기 보수적인... | Facebook", pic: "", abstract: "당에서 기본소득을 검토중이라고 한다. 김종인 대표가 기본소득을 언급한 후이고 갑자기 보수적인 색채의 정당들에서도 기본소득을 언급하면서다. 이슈가 되는 것에 대해 검토하는 것이야 정당의 기본 역할이니 당연하다. 다만 기본소득에 대한 나 개인적 입장을 말하자면 늘 말해왔듯이...", view: false, agenda_id: 1, user_id: nil}
+  {url: "http://www.econovill.com/news/articleView.html?idxno=294395", title: "[이우철의 Be-Cause마케팅] 사회 안전망과 코즈 마케팅 - 이코노믹리뷰 ", pic: "http://www.econovill.com/news/photo/201607/294395_112044_4546.jpg", abstract: "요즘 초등학생들의 장래 희망 1위가 아이돌이라고 한다. 그것을 대변하듯 대한민국에는 약 50만명의 아이돌 연습생이 있다. 물론 이들 중 성공하는...", view: false, agenda_id: 1, user_id: nil},
+  {url: "http://www.economist.com/news/leaders/21699907-proponents-basic-income-underestimate-how-disruptive-it-would-be-basically-flawed", title: "Basically flawed | The Economist", pic: "http://cdn.static-economist.com/sites/default/files/imagecache/original-size/images/print-edition/20160604_LDC584.png", abstract: "Proponents of a basic income underestimate how disruptive it would be", view: false, agenda_id: 1, user_id: nil},
+  {url: "http://m.media.daum.net/m/media/world/newsview/20160716175314264", title: "복지제도 있는데 왜 기본소득이 필요할까?..독일 좌파당 대표 인터뷰", pic: "http://t1.daumcdn.net/news/201607/16/khan/20160716175314151uaww.jpg", abstract: "[경향신문] 카티야 키핑 독일 좌파당 공동대표 “노동자들 힘 발휘 위해 필요” 강조 “국회의원들에게는 자본이나 이해집단의 로비에 굴복하지 말라고 세비를 지급하면서 왜 공화국의 평범한 시민들은 경제적 기반 없이 독립적일 것이라 기대하죠?” 카티야 키핑 독일 좌파당 공동대표(38)가 11일 <주간경향>과의 인터뷰에서 기본소득과 관련해 가장 많이 언급한 단어는", view: false, agenda_id: 1, user_id: nil}
 ])
 Url.create!([
-  {url: "https://www.facebook.com/jodeng78/posts/1069591639777202", title: "조성주 - 당에서 기본소득을 검토중이라고 한다. 김종인 대표가 기본소득을 언급한 후이고 갑자기 보수적인... | Facebook", pic: nil, abstract: "당에서 기본소득을 검토중이라고 한다. 김종인 대표가 기본소득을 언급한 후이고 갑자기 보수적인 색채의 정당들에서도 기본소득을 언급하면서다. 이슈가 되는 것에 대해 검토하는 것이야 정당의 기본 역할이니 당연하다. 다만 기본소득에 대한 나 개인적 입장을 말하자면 늘 말해왔듯이..."}
+  {url: "http://www.econovill.com/news/articleView.html?idxno=294395", title: "[이우철의 Be-Cause마케팅] 사회 안전망과 코즈 마케팅 - 이코노믹리뷰 ", pic: "http://www.econovill.com/news/photo/201607/294395_112044_4546.jpg", abstract: "요즘 초등학생들의 장래 희망 1위가 아이돌이라고 한다. 그것을 대변하듯 대한민국에는 약 50만명의 아이돌 연습생이 있다. 물론 이들 중 성공하는..."},
+  {url: "http://www.economist.com/news/leaders/21699907-proponents-basic-income-underestimate-how-disruptive-it-would-be-basically-flawed", title: "Basically flawed | The Economist", pic: "http://cdn.static-economist.com/sites/default/files/imagecache/original-size/images/print-edition/20160604_LDC584.png", abstract: "Proponents of a basic income underestimate how disruptive it would be"},
+  {url: "http://m.media.daum.net/m/media/world/newsview/20160716175314264", title: "복지제도 있는데 왜 기본소득이 필요할까?..독일 좌파당 대표 인터뷰", pic: "http://t1.daumcdn.net/news/201607/16/khan/20160716175314151uaww.jpg", abstract: "[경향신문] 카티야 키핑 독일 좌파당 공동대표 “노동자들 힘 발휘 위해 필요” 강조 “국회의원들에게는 자본이나 이해집단의 로비에 굴복하지 말라고 세비를 지급하면서 왜 공화국의 평범한 시민들은 경제적 기반 없이 독립적일 것이라 기대하죠?” 카티야 키핑 독일 좌파당 공동대표(38)가 11일 <주간경향>과의 인터뷰에서 기본소득과 관련해 가장 많이 언급한 단어는"},
+  {url: "http://www.economist.com/news/leaders/21699907-proponents-basic-income-underestimate-how-disruptive-it-would-be-basically-flawed", title: "Basically flawed | The Economist", pic: "http://cdn.static-economist.com/sites/default/files/imagecache/original-size/images/print-edition/20160604_LDC584.png", abstract: "Proponents of a basic income underestimate how disruptive it would be"}
 ])
